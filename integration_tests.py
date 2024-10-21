e7a5a02cdc113d00b1eb06292cb4c87dfd318889
sbagale148 <sbagale148@gmail.com>
2024-10-21 18:28:14 -0500

    Update integration_tests.py

--- integration_tests.py
+++ integration_tests.py
@@ -31,7 +31,7 @@ class TestSearch(TestCase):
     @patch('builtins.input')
     def test_empty_keyword(self, input_mock):
         keyword = ''
-        advanced_option_1 = 6
+        advanced_option = 6
 
         output = get_print(input_mock, [keyword, advanced_option])
         expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
@@ -99,6 +99,37 @@ class TestSearch(TestCase):
         expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid', 'Black dog (ghost)', '2007 Bulldogs RLFC season', 'Mexican dog-faced bat', 'Dalmatian (dog)', 'Guide dog', '2009 Louisiana Tech Bulldogs football team', 'Georgia Bulldogs football', 'Endoglin', 'Sun dog', 'The Mandogs', 'Georgia Bulldogs football under Robert Winston', 'Landseer (dog)']\nYour favorite article is not in the returned articles!\n"
 
         self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_max_number_of_articles(self, input_mock):
+        keyword = 'dog'
+        advanced_option = 2
+        max_articles = 5
+
+        output = get_print(input_mock, [keyword, advanced_option, max_articles])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid', 'Black dog (ghost)', '2007 Bulldogs RLFC season']\n"
+
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_random_article(self, input_mock):
+        keyword = 'dog'
+        advanced_option = 3
+        index = 2
+
+        output = get_print(input_mock, [keyword, advanced_option, index])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid']\n"
+
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_multiple_keywords(self, input_mock):
+        keyword = 'dog'
+        advanced_option = 5
+        additional_keyword = 'bat'
+
+        output = get_print(input_mock, [keyword, advanced_option, additional_keyword])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid', 'Black dog (ghost)', '2007 Bulldogs RLFC season', 'Mexican dog-faced bat', 'Dalmatian (dog)', 'Guide dog']\n
 
 # Write tests above this line. Do not remove.
 if __name__ == "__main__":
