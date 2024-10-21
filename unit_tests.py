1b8fef8b4f061d5ce38dcfb1de4882cad5e9751d
Anjolatop <anjolaoluwaodomo@gmail.com>
2024-10-21 18:27:40 -0500

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,4 @@
-    def test_search(self):
+def test_search(self):
         expected_dog_search_results = ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid', 'Black dog (ghost)', '2007 Bulldogs RLFC season', 'Mexican dog-faced bat', 'Dalmatian (dog)', 'Guide dog', '2009 Louisiana Tech Bulldogs football team', 'Georgia Bulldogs football', 'Endoglin', 'Sun dog', 'The Mandogs', 'Georgia Bulldogs football under Robert Winston', 'Landseer (dog)'] 
         expected_music_search_results = ['List of Canadian musicians', 'French pop music', 'Noise (music)', '1922 in music', '1986 in music', '2009 in music', 'Rock music', 'Lights (musician)', 'List of soul musicians', 'Aube (musician)', 'List of overtone musicians', 'Tim Arnold (musician)', 'Peter Brown (music industry)', 'Old-time music', 'Arabic music', 'List of Saturday Night Live musical sketches', 'Joe Becker (musician)', 'Aco (musician)', 'Geoff Smith (British musician)', 'Richard Wright (musician)', 'Voice classification in non-classical music', '1936 in music', '1962 in country music', 'List of dystopian music, TV programs, and games', 'Steve Perry (musician)', 'David Gray (musician)', 'Annie (musical)', 'Alex Turner (musician)', 'List of gospel musicians', 'Tom Hooper (musician)', 'Indian classical music', '1996 in music', 'Joseph Williams (musician)', 'The Hunchback of Notre Dame (musical)', 'English folk music (1500–1899)', 'David Levi (musician)', 'George Crum (musician)', 'Traditional Thai musical instruments', 'Charles McPherson (musician)', 'Les Cousins (music club)', 'Paul Carr (musician)', '2006 in music', 'Sean Delaney (musician)', 'Tony Kaye (musician)', 'Danja (musician)', 'Texture (music)', 'Register (music)', '2007 in music', '2008 in music']
         expected_animal_search_results = []
@@ -10,28 +10,27 @@
         self.assertEqual(search("    a     "),expected_aandA_search_results)
         self.assertEqual(search("A"),expected_aandA_search_results)
         self.assertEqual(search("List of Canadian musicians"), ['List of Canadian musicians'])
+    
 
     def test_title_length(self):
         expected_answer = ['short', 'longer', 'medium']
         self.assertEqual(title_length(10, ["         short", "longer", "medium", "longer than usual", "longer than usualer"]), expected_answer) 
         self.assertEqual(title_length(0, ["         short", "longer", "medium", "longer than usual", "longer than usualer"]), [])
         self.assertEqual(title_length(100, ["         short", "longer", "medium", "longer than usual", "longer than usualer"]), ["short", "longer", "medium", "longer than usual", "longer than usualer"])
-        self.assertEqual(title_length(10, ["        ", "longer", "medium", "longer than usual", "longer than usualer"]),['longer', 'medium'])
+        self.assertEqual(title_length(10, ["longer", "medium", "longer than usual", "longer than usualer"]),['longer', 'medium'])
         self.assertEqual(title_length(10, []), [])
         self.assertEqual(title_length(-5, ["hellooooooooooooooooooooooo"]), [])
-      
+
     def test_article_count(self):
         self.assertEqual(article_count(-5, []), [])
-        self.assertEqual(article_count(3, ["         ", "longer", "medium", "longer than usual", "longer than usualer"]), ["longer", "medium", "longer than usual"] )
-        self.assertEqual(article_count(8, ["    ", "longer", "medium", "longer than usual", "longer than usualer"]),['longer', 'medium', 'longer than usual', 'longer than usualer'])
-        self.assertEqual(article_count(0, ["    ", "longer", "medium", "longer than usual", "longer than usualer"]),[])
-        self.assertEqual(article_count(1, ["    ", "longer", "medium", "longer than usual", "longer than usualer"]),['longer'])
-        self.assertEqual(article_count(1, "hello",), [])
+        self.assertEqual(article_count(3, ["longer", "medium", "longer than usual", "longer than usualer"]), ["longer", "medium", "longer than usual"] )
+        self.assertEqual(article_count(8, ["longer", "medium", "longer than usual", "longer than usualer"]),['longer', 'medium', 'longer than usual', 'longer than usualer'])
+        self.assertEqual(article_count(0, ["longer", "medium", "longer than usual", "longer than usualer"]),[])
+        self.assertEqual(article_count(1, ["longer", "medium", "longer than usual", "longer than usualer"]),['longer'])
         self.assertEqual(article_count(8, []), [])
         self.assertEqual(article_count(4, ["longer", "medium", "longer than usual", "longer than usualer"]), ["longer", "medium", "longer than usual", "longer than usualer"])
-        self.assertEqual(article_count(2, [ "           ", "     longer        ", "medium", "longer than usual", "longer than usualer"]), ['longer', 'medium'])
-        self.assertEqual(article_count(2, [ "      ", "", "           "]), [])
-
+        self.assertEqual(article_count(2, ["     longer        ", "medium", "longer than usual", "longer than usualer"]), ["     longer        ", "medium"])
+    
     def test_random_article(self):
         expected_dog_search_results = ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid', 'Black dog (ghost)', '2007 Bulldogs RLFC season', 'Mexican dog-faced bat', 'Dalmatian (dog)', 'Guide dog', '2009 Louisiana Tech Bulldogs football team', 'Georgia Bulldogs football', 'Endoglin', 'Sun dog', 'The Mandogs', 'Georgia Bulldogs football under Robert Winston', 'Landseer (dog)']
         self.assertEqual(random_article(5, expected_dog_search_results), 'Mexican dog-faced bat')
