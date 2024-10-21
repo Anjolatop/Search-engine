251d462def449a6f43478fbb6cd1dd1c55a1e008
sbagale148 <sbagale148@gmail.com>
2024-10-21 17:26:25 -0500

    Create integration_tests.py

---
+++ integration_tests.py
@@ -0,0 +1,83 @@
+from search import search, title_length, article_count, random_article, favorite_article, multiple_keywords, display_result
+from search_tests_helper import get_print, print_basic, print_advanced, print_advanced_option
+from wiki import article_titles
+from unittest.mock import patch
+from unittest import TestCase, main
+
+class TestSearch(TestCase):
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
+    #####################
+    # INTEGRATION TESTS #
+    #####################
+
+    @patch('builtins.input')
+    def test_example_integration_test(self, input_mock):
+        keyword = 'dog'
+        advanced_option = 6
+
+        # Output of calling display_results() with given user input. If a different
+        # advanced option is included, append further user input to this list (after `advanced_option`)
+        output = get_print(input_mock, [keyword, advanced_option])
+        # Expected print outs from running display_results() with above user input
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: ['Edogawa, Tokyo', 'Kevin Cadogan', 'Endogenous cannabinoid', 'Black dog (ghost)', '2007 Bulldogs RLFC season', 'Mexican dog-faced bat', 'Dalmatian (dog)', 'Guide dog', '2009 Louisiana Tech Bulldogs football team', 'Georgia Bulldogs football', 'Endoglin', 'Sun dog', 'The Mandogs', 'Georgia Bulldogs football under Robert Winston', 'Landseer (dog)']\n"
+
+        # Test whether calling display_results() with given user input equals expected printout
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_empty_keyword(self, input_mock):
+        keyword = ''
+        advanced_option_1 = 6
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_whitespace_keyword(self, input_mock):
+        keyword = '    '
+        advanced_option = 6
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_non_existent_keyword(self, input_mock):
+        keyword = 'unicorn'
+        advanced_option = 6
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_case_insensitivity(self, input_mock):
+        keyword = 'DOG'
+        advanced_option = 6
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: [‘Edogawa, Tokyo’, ‘Kevin Cadogan’, ‘Endogenous cannabinoid’, ‘Black dog (ghost)’, ‘2007 Bulldogs RLFC season’, ‘Mexican dog-faced bat’, ‘Dalmatian (dog)’, ‘Guide dog’, ‘2009 Louisiana Tech Bulldogs football team’, ‘Georgia Bulldogs football’, ‘Endoglin’, ‘Sun dog’, ‘The Mandogs’, ‘Georgia Bulldogs football under Robert Winston’, ‘Landseer (dog)’]\n"
+
+        self.assertEqual(output, expected)
+
+    @patch('builtins.input')
+    def test_special_characters_keyword(self, input_mock):
+        keyword = '@#%!&'
+        advanced_option = 6
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+
+# Write tests above this line. Do not remove.
+if __name__ == "__main__":
+    main()
