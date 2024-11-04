1d790c6bb3412fa2b13b76ff7101b4f6e40ced72
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-11-03 22:13:07 -0600

    Update integration_tests_part2.py

--- integration_tests_part2.py
+++ integration_tests_part2.py
@@ -1,1 +1,8 @@
+from search import search, article_length, unique_authors, most_recent_article, favorite_author, title_and_author, refine_search, display_result
+from search_tests_helper import get_print, print_basic, print_advanced, print_advanced_option
+from wiki import article_metadata
+from unittest.mock import patch
+from unittest import TestCase, main
+
+
 
