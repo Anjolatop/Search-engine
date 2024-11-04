90e0bd8122ad8ef2fa936495c5c9e7779b19e948
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-11-04 16:15:47 -0600

    Update integration_tests_part2.py

--- integration_tests_part2.py
+++ integration_tests_part2.py
@@ -4,5 +4,235 @@ from wiki import article_metadata
 from unittest.mock import patch
 from unittest import TestCase, main
 
+class TestSearch(TestCase):
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
+    def test_example_unit_test(self):
+        expected_search_soccer_results = [
+            ['Spain national beach soccer team', 'jack johnson', 1233458894, 1526],
+            ['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562],
+            ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]
+        ]
+        self.assertEqual(search('soccer'), expected_search_soccer_results)
+
+    #####################
+    # INTEGRATION TESTS #
+    #####################
+
+    @patch('builtins.input')
+    def test_example_integration_test(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 1
+        advanced_response = 3000
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\n"
+
+        self.assertEqual(output, expected)
+
+# Write tests above this line. Do not remove.
+    @patch('builtins.input')
+    def test_integration_test_basic_search1(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 7
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\n"
+        
+        self.assertEqual(output, expected)
+
+    
+    @patch('builtins.input')
+    def test_integration_test_basic_search2(self, input_mock):
+        keyword = ''
+        advanced_option = 7
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
+
+        self.assertEqual(output, expected)
 
 
+    @patch('builtins.input')
+    def test_integration_test_basic_search3(self, input_mock):
+        keyword = 'jermaine'
+        advanced_option = 7
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search1_1(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 1
+        advanced_response = 3000
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search1_2(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 1
+        advanced_response = 1
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search2_1(self, input_mock):
+        keyword = 'the'
+        advanced_option = 2
+        advanced_response = 3
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['List of Canadian musicians', 'Jack Johnson', 1181623340, 21023], ['French pop music', 'Mack Johnson', 1172208041, 5569], ['1922 in music', 'Gary King', 1242717698, 11576]]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search2_2(self, input_mock):
+        keyword = 'and'
+        advanced_option = 2
+        advanced_response = 9000
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['List of Canadian musicians', 'Jack Johnson', 1181623340, 21023], ['French pop music', 'Mack Johnson', 1172208041, 5569], ['1922 in music', 'Gary King', 1242717698, 11576], ['Kevin Cadogan', 'Mr Jake', 1144136316, 3917], ['2009 in music', 'RussBot', 1235133583, 69451], ['Lights (musician)', 'Burna Boy', 1213914297, 5898], ['Human computer', 'Bearcat', 1248275178, 4750], ['Black dog (ghost)', 'Pegship', 1220471117, 14746], ['Old-time music', 'Nihonjoe', 1124771619, 12755]]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search2_3(self, input_mock):
+        keyword = 'and'
+        advanced_option = 2
+        advanced_response = 0
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nNo articles found\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search3(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 3
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search4_1(self, input_mock):
+        keyword = 'jermaine'
+        advanced_option = 4
+        advanced_response = 'jermaine'
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nNo articles found\nYour favorite author is not in the returned articles!\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search4_2(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 4
+        advanced_response = 'jack johnson'
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\nYour favorite author is in the returned articles!\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search4_3(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 4
+        advanced_response = 'jermaine'
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\nYour favorite author is not in the returned articles!\n"
+
+        self.assertEqual(output, expected)
+
+    
+    @patch('builtins.input')
+    def test_integration_test_advanced_search5(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 5
+
+        output = get_print(input_mock, [keyword, advanced_option])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + "\nHere are your articles: [('Spain national beach soccer team', 'jack johnson'), ('Will Johnson (soccer)', 'Burna Boy'), ('Steven Cohen (soccer)', 'Mack Johnson')]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search6_1(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 6
+        advanced_response = 'his'
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search6_2(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 6
+        advanced_response = 'soccer'
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\n"
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search6_3(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 6
+        advanced_response = ''
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + '\n\nNo articles found\n'
+
+        self.assertEqual(output, expected)
+    
+
+    @patch('builtins.input')
+    def test_integration_test_advanced_search6_4(self, input_mock):
+        keyword = ''
+        advanced_option = 6
+        advanced_response = 'soccer'
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + '\n\nNo articles found\n'
+
+        self.assertEqual(output, expected)
+
+        
+    
+if __name__ == "__main__":
+    main()
