50316e5449c913d53d6c0d4caba6bc1667ff18ad
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-11-18 21:01:04 -0600

    Update unit_tests_part3.py

--- unit_tests_part3.py
+++ unit_tests_part3.py
@@ -29,3 +29,17 @@ class TestKeywordToTitles(unittest.TestCase):
       expected_output = {}
       self.assertEqual(keyword_to_titles(metadata), expected_output)
 
+
+
+    def test_normal_case(self):
+        metadata = [
+            ("Title1", "Author1", "2023-01-01", 500),
+            ("Title2", "Author2", "2023-01-02", 1000),
+            ("Title3", "Author3", "2023-01-03", 750),
+        ]
+        expected_output = {
+            "Title1": {"author": "Author1", "timestamp": "2023-01-01", "length": 500},
+            "Title2": {"author": "Author2", "timestamp": "2023-01-02", "length": 1000},
+            "Title3": {"author": "Author3", "timestamp": "2023-01-03", "length": 750},
+        }
+        self.assertEqual(title_to_info(metadata), expected_output)
